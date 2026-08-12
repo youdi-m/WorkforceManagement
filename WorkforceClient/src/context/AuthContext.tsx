f88c81aa891dd10762673856a53c790c0e272120
youdi <mehdi.youssef97@gmail.com>
2026-08-12 11:44:31 -0400

    implemented roles in frontend for ux

---
+++ WorkforceClient/src/context/AuthContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react"
+import type { ReactNode } from "react";
+
+// declating shape
+interface AuthContextType {
+	role: string | null
+	setRole: (role: string | null) => void
+}
+
+// declaring undefined context
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+// function to wrap the app
+export function AuthProvider({children} : {children: ReactNode}){
+	const [role, setRole] = useState<string | null>(null)
+
+	return (
+		// hold the role and provides it to wrapped children
+		<AuthContext.Provider value={{role, setRole}}>
+			{children}
+		</AuthContext.Provider>
+	)
+}
+
+// function to read the contexts current value
+export function useAuth() {
+	const context = useContext(AuthContext)
+	// handle using useAuth outsite the AuthProvider
+	if(context == undefined) {
+		throw new Error('useAuth must be used within an AuthProvider')
+	}
+	return context
+}
--- WorkforceClient/src/pages/Login.tsx
+++ WorkforceClient/src/pages/Login.tsx
@@ -1,16 +1,17 @@
-
 import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 function Login() {
 	const navigate = useNavigate()
+	const {setRole} = useAuth()
 
 	return (
 		// temporary fake login buttons
 		<div>
 			<h1>Login</h1>
-			<button onClick={() => navigate('/employee')}>Employee</button>
-			<button onClick={() => navigate('/lead')}>Lead</button>
-			<button onClick={() => navigate('/hr')}>Hr</button>
+			<button onClick={() => {setRole('employee'); navigate('/employee')}}>Employee</button>
+			<button onClick={() => {setRole('lead'); navigate('/lead')}}>Lead</button>
+			<button onClick={() => {setRole('hr'); navigate('/hr')}}>Hr</button>
 		</div>
 	)
 }
